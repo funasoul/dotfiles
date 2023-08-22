6315d16a66d1ef4007098a45a58bf31035860e8e
Akira Funahashi <funa@bio.keio.ac.jp>
2023-08-23 05:28:06 +0900

    clean up

--- .config/kitty/id_history.py
+++ .config/kitty/id_history.py
@@ -5,7 +5,6 @@ from kitty.boss import Boss
 def main(args: List[str]) -> str:
     # this is the main entry point of the kitten, it will be executed in
     # the overlay window when the kitten is launched
-    #answer = input('Enter some text: ')
     # whatever this function returns will be available in the
     # handle_result() function
     window_id = "0"
@@ -20,11 +19,8 @@ def main(args: List[str]) -> str:
 def handle_result(args: List[str], answer: str, target_window_id: int, boss: Boss) -> None:
     # get the kitty window into which to paste answer
     w = boss.window_id_map.get(target_window_id)
-    if w is not None and answer != "0" and answer != target_window_id:
+    if w is not None and answer != "0":
         with open(os.path.expanduser('~') + '/.config/kitty/last_window_id.txt', 'w') as id_file:
             id_file.write(str(target_window_id))
-        #boss.call_remote_control(w, ('focus-tab', '--match=title:core'))
         if args[1] == "last_window":
             boss.call_remote_control(w, ('focus-tab', f'--match=id:{answer}'))
-        #boss.call_remote_control(w, ('focus-tab', '--match=id:5'))
-        #boss.call_remote_control(w, ('send-text', f'--match=id:{w.id}', 'here'))
