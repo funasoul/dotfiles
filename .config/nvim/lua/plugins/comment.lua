c5814f7e558b0bacffb519f4e9b9284540b03225
Akira Funahashi <funa@bio.keio.ac.jp>
2023-08-23 14:57:05 +0900

    Changed to use comment_toggle_linewise instead of blockwise, because python does not support blockwise comment

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -3,8 +3,8 @@ return {
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "<leader>.", "<Plug>(comment_toggle_blockwise_current)", mode = "n", desc = "Comment" },
-      { "<leader>.", "<Plug>(comment_toggle_blockwise_visual)", mode = "v", desc = "Comment" },
+      { "<leader>.", "<Plug>(comment_toggle_linewise_current)", mode = "n", desc = "Comment" },
+      { "<leader>.", "<Plug>(comment_toggle_linewise_visual)", mode = "v", desc = "Comment" },
     },
   },
 }
