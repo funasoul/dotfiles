f12e57705ecb9212f3e01ce0976f1bffa6130a31
Akira Funahashi <funa@bio.keio.ac.jp>
2023-08-23 14:43:14 +0900

    Do not jump on invocation of *. https://github.com/LazyVim/LazyVim/pull/1343/commits/cb151005e1ae3b142f895c73b8c66192f453c925

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Do not jump on invocation of *
+vim.keymap.set("n", "*", ":keepjumps normal! mi*`i<Cr>")
