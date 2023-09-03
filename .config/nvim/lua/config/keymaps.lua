2d8cca638af9955c3c9c22a24d5301a5a4d09d85
Akira Funahashi <funa@bio.keio.ac.jp>
2023-09-03 21:50:32 +0900

    <C-n>, <C-p> will jump to next/previous error/warning. I need this!

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,7 @@
 
 -- Do not jump on invocation of *
 vim.keymap.set("n", "*", ":keepjumps normal! mi*`i<Cr>")
+
+-- <C-n>, <C-p> will jump to next/previous error
+vim.keymap.set("n", "<C-n>", "<cmd>lua vim.diagnostic.goto_next{wrap = false}<cr>")
+vim.keymap.set("n", "<C-p>", "<cmd>lua vim.diagnostic.goto_prev{wrap = false}<cr>")
