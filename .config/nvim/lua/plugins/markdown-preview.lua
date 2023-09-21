a2fbccaf47758a6f7d5eb80f8ffab28fee0059b3
Akira Funahashi <funa@bio.keio.ac.jp>
2023-09-21 19:35:45 +0900

    lazy load markdown-preview

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -2,6 +2,7 @@ return {
   -- Preview markdown on your modern browser with synchronised scrolling and flexible configuration
   {
     "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
     config = function()
       vim.fn["mkdp#util#install"]()
     end,
