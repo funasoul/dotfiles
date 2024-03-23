6a8cc3b6d67d858957921a737886edf4b39d639e
Akira Funahashi <funa@bio.keio.ac.jp>
2024-03-24 07:58:53 +0900

    Add oil.lua

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  -- A vim-vinegar like file explorer that lets you edit your filesystem like a normal Neovim buffer.
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+      },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
