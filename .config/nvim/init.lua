5e1d9f47b651d3e693a8f8a46e9c4076871dcf7f
Akira Funahashi <funa@bio.keio.ac.jp>
2024-08-06 20:37:28 +0900

    Moved my options from init.lua to options.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,13 +2,3 @@
 require("config.lazy")
 -- load my custom functions
 require("custom.functions")
--- my options
-vim.opt.backup = false
-vim.opt.writebackup = false
-vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = false
-vim.opt.wrapscan = false
-vim.opt.shiftround = true
-vim.opt.showmatch = true
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = false
+vim.opt.wrapscan = false
+vim.opt.shiftround = true
+vim.opt.showmatch = true
