705ee904e292be9d425a0ded99d9c5427423118f
Akira Funahashi <funa@bio.keio.ac.jp>
2025-09-24 17:40:45 +0900

    Call pokemon-colorscripts (https://gitlab.com/phoneybadger/pokemon-colorscripts) on launch

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,33 @@
+return {
+  "snacks.nvim",
+  opts = {
+    dashboard = {
+      sections = vim.fn.executable("pokemon-colorscripts") == 1
+          and {
+            {
+              section = "terminal",
+              cmd = "pokemon-colorscripts --no-title -rn bulbasaur,charmander,dratini,eevee,growlithe,mew,mimikyu,pachirisu,pancham,pichu,pikachu,piplup,psyduck,shaymin,skitty,squirtle,togepi,torchic,yamper",
+              height = 10,
+              padding = 1,
+            },
+            { section = "keys", gap = 1, padding = 1 },
+            { section = "startup" },
+          }
+        or {
+          {
+            text = [[
+        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+ ]],
+            padding = 1,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+    },
+  },
+}
