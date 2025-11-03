17428301ff5b4e68d782f70fe7a84b63703e9c90
Akira Funahashi <funa@bio.keio.ac.jp>
2025-11-04 03:32:14 +0900

    disabled octo. now we can use snack instead.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "pwntester/octo.nvim", enabled = false },
   { "stevearc/oil.nvim", enabled = false },
   { "folke/flash.nvim", enabled = true },
   { "frankroeder/parrot.nvim", enabled = false },
