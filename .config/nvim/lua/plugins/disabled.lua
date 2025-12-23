064438f7c590262f20e90be36c8bb0190f35e1bd
Akira Funahashi <funa@bio.keio.ac.jp>
2025-12-23 20:25:21 +0900

    feat(plugins): enable flash.nvim plugin

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "stevearc/oil.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = true },
   { "frankroeder/parrot.nvim", enabled = false },
   { "supermaven-inc/supermaven-nvim", enabled = false },
   { "olimorris/codecompanion.nvim", enabled = false },
