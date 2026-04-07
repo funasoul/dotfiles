aa2fdf49921070d4af4e292263b417d2ebd88550
Akira Funahashi <funa@bio.keio.ac.jp>
2026-04-08 02:44:15 +0900

    fix(terminal): disable [Process exited 0] message in Neovim >= 0.12.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,6 @@
 require("config.lazy")
 -- load my custom functions
 require("custom.functions")
+
+-- disable showing [Process exited 0] under my pokemon (works on nvim >= 0.12.0)
+vim.api.nvim_clear_autocmds({ group = "nvim.terminal", event = "TermClose" })
