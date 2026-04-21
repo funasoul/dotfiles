62f586a38271bff2821d2ef5869eb8ea7d6dc9c1
Akira Funahashi <funa@bio.keio.ac.jp>
2026-04-22 00:42:35 +0900

    fix(kitty): disabled plugin check-update on copy-mode

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = vim.env.LAZY_CHECKER ~= "0" }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
